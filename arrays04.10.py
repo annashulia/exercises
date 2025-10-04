80693a230bcf1e67dd24945d37ea3948ee8e3e6e
annashulia <ania.solea2@gmail.com>
2025-10-04 16:21:48 +0300

    Update arrays04.10.py

--- arrays04.10.py
+++ arrays04.10.py
@@ -6,24 +6,35 @@ arr2 = array.array('i', [4, 5, 6, 7, 8])
 arr1.append(4)
 arr2.extend([9, 10, 11])
 
-print("After arr1.append(4), arr1 is:", arr1)
-print(arr2)
+print("After adding 4 to arr1:", list(arr1))
+print("After adding elements to arr2:", list(arr2))
 
 
 index = 3
-removed = arr2.pop(index)
+removed = arr1.pop(index)
 
 arr2.remove(5)
 
-print("Removed from arr2 at index", index, ":", removed)
-print("arr2 now:", list(arr2))
-print("arr2 after remove(5):", list(arr2))
-
-arr1 = array.array('i', sorted(arr1))
-arr2 = array.array('i', sorted(arr2))
-print("arr1 sorted:", list(arr1))
-print("arr2 sorted:", list(arr2))
+print("Removed 1 from arr1 using index", list(arr1))
+print("arr2 after removing 5:", list(arr2))
 
 
 
+def bubble_sorted(a: array.array) -> array.array:
+    b = array.array(a.typecode, a)  
+    n = len(b)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n-1-i):
+            if b[j] > b[j+1]:
+                b[j], b[j+1] = b[j+1], b[j]
+                swapped = True
+        if not swapped:
+            break
+    return b
+
+arr = array.array('i', [5, 3, 8, 1, 2])
+print("Original:", list(arr))
+sorted_arr = bubble_sorted(arr)
+print("Sorted (bubble):", list(sorted_arr))
 
